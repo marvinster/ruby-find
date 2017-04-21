d4c511d35207851564e8de4733ae5cf2b478f752
Pavel Zaitsev <pavel@arslogi.ca>
2017-04-21 10:53:04 +0300

    codecov badgew

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ![status](https://travis-ci.org/pavelz/ruby-find.svg?branch=master)
+[![codecov](https://codecov.io/gh/pavelz/ruby-find/branch/master/graph/badge.svg)](https://codecov.io/gh/pavelz/ruby-find)
+
 # Some EventMachine magic
--- server/find.rb
+++ server/find.rb
@@ -4,6 +4,7 @@ class FindServer < EventMachine::Connection
   end
   def receive_data data
     send_data ">> you sent #{data}\n"
+   send_data User.first.inspect.to_s + "\n"
     close_connection if data =~ /quit/i
   end
   def unbind
