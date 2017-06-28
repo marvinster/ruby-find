07614a65b7cec2a1d76d6bc5da5f339ca2a53543
Pavel Zaitsev <pavel@arslogi.ca>
2017-06-29 00:01:43 +0300

    simplify

--- spec/server/server_spec.rb
+++ spec/server/server_spec.rb
@@ -2,17 +2,9 @@ require 'spec_helper.rb'
 require_relative '../../server/find'
 require 'awesome_print'
 RSpec.describe "findserver doing ssl connection"  do
-  before :all do
-    # make connection object here outside event machine
-    class EventMachine::Connection
-      def new(one, two, *args)
-      end
-      def start_tls(*args)
-      end
-    end
-  end
   subject{FindServer.new(1,2)}
   it 'should serve ssl on port 9999' do
+    allow_any_instance_of(FindServer).to receive(:start_tls)
     expect(subject).to receive(:start_tls).with(any_args)
     subject.post_init()
   end
