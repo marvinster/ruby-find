8e41b687f1251d377f31bb34be88c8c7e4f43b5a
Pavel Zaitsev <pavel@arslogi.ca>
2017-06-28 22:55:06 +0300

    finally thought my way into mocking produced function

--- spec/server/server_spec.rb
+++ spec/server/server_spec.rb
@@ -4,14 +4,17 @@ require 'awesome_print'
 RSpec.describe "findserver doing ssl connection"  do
   before :all do
     # make connection object here outside event machine
-    
+    class EventMachine::Connection
+      def new(one, two, *args)
+      end
+      def start_tls(*args)
+      end
+    end
   end
+  subject{FindServer.new(1,2)}
   it 'should serve ssl on port 9999' do
     expect(subject).to receive(:start_tls).with(any_args)
-    EventMachine.run do
-      EventMachine.add_timer(2){ EventMachine.stop_server }
-      EventMachine.start_server 'ldev.arslogi.ca',10000,FindServer
-    end
+    subject.post_init()
   end
 end
 
